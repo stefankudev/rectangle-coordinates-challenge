9a8003a224960e380e629d6ab06bdb15234156ed
Stefan Kudev <19394245+stefankudev@users.noreply.github.com>
2025-04-07 20:03:31 +0000

    wip: working test

---
+++ index.test.ts
@@ -0,0 +1,12 @@
+import {describe, test, expect} from 'vitest';
+import {findOverlappingCoordinates} from './index'
+
+describe('find overlapping coordinates function', () => {
+    test("given arguments of [[-4,1],[8,6]] and [[1,-1],[4,7]] returns [[1,1],[4,6]]", () => {
+        const yellowRectangle = [[-4,1],[8,6]];
+        const blueRectangle = [[1,-1],[4,7]];
+
+        const result = findOverlappingCoordinates([yellowRectangle,blueRectangle])
+        expect(result).toBe([[1,1],[4,6]]);
+    })
+  })
---
+++ index.ts
@@ -0,0 +1,8 @@
+type CoordinatesArray = number[][];
+type RectangleCoordinates = [CoordinatesArray,CoordinatesArray]
+type Result = CoordinatesArray
+
+export const findOverlappingCoordinates = (rectangles: RectangleCoordinates): Result => {
+    const [rectangle1, rectangle2] = rectangles;
+    return [];
+}
