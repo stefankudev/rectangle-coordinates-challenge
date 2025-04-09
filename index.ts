60b1e4bdea451a7e32792d12b6759b61a7c79b26
Stefan Kudev <19394245+stefankudev@users.noreply.github.com>
2025-04-09 08:44:28 +0000

    chore: update types

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-type CoordinatesArray = number[][];
-type RectangleCoordinates = [CoordinatesArray,CoordinatesArray]
-type Result = CoordinatesArray
+export type CoordinatesArray = number[][];
+export type RectangleCoordinates = [CoordinatesArray,CoordinatesArray]
 
-export const findOverlappingCoordinates = (rectangles: RectangleCoordinates): Result => {
+export const findOverlappingCoordinates = (rectangles: RectangleCoordinates): CoordinatesArray => {
     const [rectangle1, rectangle2] = rectangles;
     const [r1x, r1y] = rectangle1;
     const [r2x, r2y] = rectangle2;
+
+    // TODO: What if there is no overlap?
+
     const result = [[r2x[0],r1x[1]],[r2y[0],r1y[1]]]
     return result;
 }
