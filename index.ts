6ee6e3a174cc0de5b7e64c837f8efc6c32e382b0
Stefan Kudev <19394245+stefankudev@users.noreply.github.com>
2025-04-14 16:49:15 +0000

    feat: return null if no overlap

--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
 export type CoordinatesArray = number[][];
 export type RectangleCoordinates = [CoordinatesArray,CoordinatesArray]
+export type Result = CoordinatesArray | null;
 
-export const findOverlapCoordinates = (rectangles: RectangleCoordinates): CoordinatesArray => {
+export const findOverlapCoordinates = (rectangles: RectangleCoordinates): Result => {
     const [rectangle1, rectangle2] = rectangles;
-    const [r1x, r1y] = rectangle1;
-    const [r2x, r2y] = rectangle2;
+    const [[rect1_x1, rect1_y1], [rect1_x2, rect1_y2]] = rectangle1;
+    const [[rect2_x1, rect2_y1], [rect2_x2, rect2_y2]] = rectangle2;
 
-    // TODO: What if there is no overlap?
+    const overlap_x1 = Math.max(rect1_x1, rect2_x1);
+    const overlap_y1 = Math.max(rect1_y1, rect2_y1);
+    const overlap_x2 = Math.min(rect1_x2, rect2_x2);
+    const overlap_y2 = Math.min(rect1_y2, rect2_y2);
 
-    const result = [[r2x[0],r1x[1]],[r2y[0],r1y[1]]]
-    return result;
+    const noOverlapOnX = overlap_x1 >= overlap_x2;
+    const noOverlapOnY = overlap_y1 >= overlap_y2;
+
+    if (noOverlapOnX || noOverlapOnY) {
+        return null;
+    }
+
+    return [[overlap_x1, overlap_y1], [overlap_x2, overlap_y2]];
 }
